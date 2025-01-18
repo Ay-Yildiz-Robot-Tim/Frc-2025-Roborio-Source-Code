b7c3372e49857dd5c222dde28d32ec6d5375b86e
EfeHasan <efeg10455@gmail.com>
2025-01-18 17:40:48 +0300

    manual mode yazılım optimizasyonu

--- PwmMotorDrive/src/main/java/frc/robot/subsystems/PlatformMovement.java
+++ PwmMotorDrive/src/main/java/frc/robot/subsystems/PlatformMovement.java
@@ -1,54 +1,90 @@
 package frc.robot.subsystems;
 
+import javax.security.sasl.AuthorizeCallback;
+
 import com.fasterxml.jackson.databind.cfg.EnumFeature;
+
+import edu.wpi.first.wpilibj.PS4Controller.Axis;
 
 public class PlatformMovement {
     
-    public double[] PowerCalc(double powerFront, double powerBack, double axisX) {//index 0 right motors index 1 left motors
-        double leftPowerMotors = 0;
-        double rightPowerMotors = 0;
-        
-        //rt buttonundan gelen hızın ileri olarak atanması
-        if(powerFront > .1){
-            //default atanması gereken değer
-            leftPowerMotors = powerFront;
+    private double[] FrontLineer(double powerFront, double axisX){
+        //default atanması gereken değer
+        double leftPowerMotors = powerFront;
+        double rightPowerMotors = -powerFront;
+
+        //açısal dönüş koşulları
+        if(axisX > .1){
+            leftPowerMotors = powerFront * axisX;
             rightPowerMotors = -powerFront;
-
-            //açısal dönüş koşulları
-            if(axisX > .1){
-                leftPowerMotors = powerFront * axisX;
-                rightPowerMotors = -powerFront;
-            }
-            else if(axisX < -.1){
-                leftPowerMotors = powerFront;
-                rightPowerMotors = -powerFront * axisX;
-            }
         }
-        else if(powerBack > .1){
-            leftPowerMotors = -powerBack;
-            rightPowerMotors = powerBack;
-
-            if(axisX > .1){
-                leftPowerMotors = -powerBack;
-                rightPowerMotors = powerBack * axisX;
-            }
-            else if(axisX < -.1){
-                leftPowerMotors = -powerBack * axisX;
-                rightPowerMotors = powerBack;
-            }
-        }
-        else{
-            if(axisX > .1){
-                leftPowerMotors = -axisX;
-                rightPowerMotors = -axisX;
-            }
-            else if(axisX < -.1){
-                leftPowerMotors = -axisX;
-                rightPowerMotors = -axisX;
-            }
+        else if(axisX < -.1){
+            leftPowerMotors = powerFront;
+            rightPowerMotors = -powerFront * axisX;
         }
 
         return new double[]{rightPowerMotors, leftPowerMotors};
+    }
+
+    private double[] BackLineer(double powerBack, double axisX){
+        double leftPowerMotors = -powerBack;
+        double rightPowerMotors = powerBack;
+
+        //açısal dönüş koşulları ve hesaplanması
+        if(axisX > .1){
+            leftPowerMotors = -powerBack;
+            rightPowerMotors = powerBack * axisX;
+        }
+        else if(axisX < -.1){
+            leftPowerMotors = -powerBack * axisX;
+            rightPowerMotors = powerBack;
+        }
+
+        return new double[]{rightPowerMotors, leftPowerMotors};
+    }
+
+    //tank dönüşü güç hesaplması
+    private double[] TankTurn(double axisX){
+        /*
+        //sol dönüş ayarlamsı sağ motorlar ters bağlandığı için sağ motorlara - vermek ileri hareketi sağla
+        if(axisX > .1){
+            leftPowerMotors = -axisX;
+            rightPowerMotors = -axisX;
+        }
+        //sağ dönüş ayarlaması için sağ motorlar + da geri gidecek
+        else if(axisX < -.1){
+            leftPowerMotors = -axisX;
+            rightPowerMotors = -axisX;
+        }
+        return new double[]{rightPowerMotors, leftPowerMotors};
+        */
+
+        //tank dönüşü güç hesaplması
+        double leftPowerMotors = 0;
+        double rightPowerMotors = 0;
+
+        if(axisX < -.1 || axisX > .1){
+            leftPowerMotors = -axisX;
+            rightPowerMotors = -axisX;
+        }
+        return new double[] {rightPowerMotors, leftPowerMotors};
+    }
+
+    public double[] PowerCalc(double powerFront, double powerBack, double axisX) {//index 0 right motors index 1 left motors
+        //ileri buttonunda hareket varmı
+        if(powerFront > .1){
+            return FrontLineer(powerFront, axisX);
+        }
+
+        //geri butonunda hareket varmı
+        else if(powerBack > .1){
+            return BackLineer(powerBack, axisX);
+        }
+
+        //yön butonunda hareket varmı
+        else{
+            return TankTurn(axisX);
+        }
     }
 
     public double[] PowerCalc(int targetAngle, int currentRPM, int targetRPM, int currentAngle) {
